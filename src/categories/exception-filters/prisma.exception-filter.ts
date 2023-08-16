40736ddac9350c7a473424e601a0efbec597fcb2
Leandro Nunes <nunes.pessoal@gmail.com>
2023-08-16 20:30:46 -0300

    prisma config

---
+++ src/categories/exception-filters/prisma.exception-filter.ts
@@ -0,0 +1,7 @@
+import { Catch, ExceptionFilter } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Catch(PrismaClientKnownRequestError)
+export class PrismaExceptionFilter implements ExceptionFilter {
+  catch(excepetion, host);
+}
